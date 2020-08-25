eea58538ee43ea5b02c26c3f544d0312f4c1a7e5
Alcazar99 <11788248080@qq.com>
2020-08-25 20:45:19 +0800

    初始化数据统计分支

---
+++ src/components/reports/report.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 2. 为ECharts准备一个具备大小（宽高）的Dom -->
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
